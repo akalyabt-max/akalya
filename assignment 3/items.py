029d2e4bf66133bcf50da8dfd83d3b0938e90a25
akalyabt-max <225027005@sastra.ac.in>
2024-06-19 18:15:01 +0530

    Add files via upload
    supermarket program

---
+++ assignment 3/items.py
@@ -0,0 +1,12 @@
+items = {
+    'coffee': 100,
+    'tea': 150,
+    'milk': 25,
+    'chcolate': 50,
+    'salt': 80,
+    'rice':150,
+    'papper':30,
+    'maggi':50,
+    'boost':90,
+    'colgate':85,
+}    
